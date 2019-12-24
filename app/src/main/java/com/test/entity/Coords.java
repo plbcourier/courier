34d962c1eb03bcdddaec8304210849b85379aba2
yks <1679682913@qq.com>
2019-12-24 09:23:00 +0800

    颜恺生 添加获取坐标工具类

---
+++ app/src/main/java/com/test/entity/Coords.java
@@ -0,0 +1,36 @@
+package com.test.entity;
+
+/**
+ * Created by Administrator on 2019/12/24.
+ */
+
+//-----------坐标参数对象实体类-----------
+public class Coords {
+    private Double longitude;//经度
+    private Double latitude;//纬度
+    private String address;//地址字符串
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
