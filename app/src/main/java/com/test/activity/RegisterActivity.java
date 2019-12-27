39a506f60ff3944abf63fb32f6547b28faa85b20
yks <1679682913@qq.com>
2019-12-27 09:19:43 +0800

    颜恺生 添加注册验证码

--- app/src/main/java/com/test/activity/RegisterActivity.java
+++ app/src/main/java/com/test/activity/RegisterActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -11,6 +13,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.mob.MobSDK;
 import com.test.courier.ClearEditText;
 import com.test.courier.R;
 import com.test.entity.Constant;
@@ -20,6 +23,8 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
+import cn.smssdk.EventHandler;
+import cn.smssdk.SMSSDK;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
@@ -31,51 +36,91 @@ import okhttp3.Response;
 //--------------用户注册---------------
 public class RegisterActivity extends Activity implements View.OnClickListener {
     private ImageView backLoginPage;
-    private ClearEditText editText;
-    private ClearEditText editText1;
-    private ClearEditText editText2;
-    private Button zhuce;
+    private ClearEditText name_edit;
+    private ClearEditText phone_edit;
+    private ClearEditText pwd_edit;
+    private Button zhuce_btn;
     private Constant constant;
     private String name1;
-    private Button identifying;
+    private Button captcha_btn;
+    private EditText captcha_edit;
     private TimeCount time;
-    private Button submit;//短信验证按钮
+    private Message message;
+    private EventHandler eventHandler;//事件handler
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_register);
-        editText=findViewById(R.id.name);//获取注册名字
-        editText1=findViewById(R.id.number);//获取注册手机号码
-        editText2=findViewById(R.id.pwd);//获取注册密码
-        zhuce=findViewById(R.id.zhuce);//注册按钮
-        zhuce.setOnClickListener(this);//注册按钮点击事件
-        identifying=findViewById(R.id.up_codebt);//获取验证码按钮
-        identifying.setOnClickListener(this);
-        time=new TimeCount(60000,1000);
-        constant=new Constant();//实力换Constant类
         init();
+        MobSDK.init(this);//短信SDK初始化
+        initEventHandler();//初始化事件handler
     }
 
-    private void init() {
-        backLoginPage=findViewById(R.id.back_login_img);
-        backLoginPage.setOnClickListener(this);
+    private void initEventHandler() {//初始化事件handler
+        eventHandler = new EventHandler(){
+            @Override
+            public void afterEvent(int event, int result, Object data) {
+                if (result == SMSSDK.RESULT_COMPLETE){//回调完成
+                    if (event==SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE){//提交验证码成功
+                        //存储用户数据：
+                        message = new Message();
+                        message.what=1;
+                        handler.sendMessage(message);
+                    }else if(event==SMSSDK.EVENT_GET_VERIFICATION_CODE){//获取验证码成功
+                        /*message = new Message();
+                        message.what=2;
+                        handler.sendMessage(message);*/
+                    }else if(event==SMSSDK.EVENT_GET_SUPPORTED_COUNTRIES){//返回支持发送验证码的国家列表
+                        /*message = new Message();
+                        message.what=3;
+                        handler.sendMessage(message);*/
+                    }
+                }else{
+                    ((Throwable)data).printStackTrace();//回调出错，控制台打印错误信息
+                }
+            }
+        };
+        SMSSDK.registerEventHandler(eventHandler);//注册短信回调
     }
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what==1){//验证成功
+                String namestr=name_edit.getText().toString();
+                String phonestr=phone_edit.getText().toString();
+                String pwdstr=pwd_edit.getText().toString();
+                registerWithOkHttp(namestr,phonestr,pwdstr);
+            }else if(msg.what==2){
+
+            }
+        }
+    };
 
     @Override
     public void onClick(View v) {
+        String phone = null;
         switch (v.getId()){
-            case R.id.back_login_img:
+            case R.id.back_login_img://返回按钮
                 finish();
                 break;
-            case R.id.zhuce:
-                String editText_name=editText.getText().toString();
-                String editText_number=editText1.getText().toString();
-                String editText_pwd=editText2.getText().toString();
-                registerWithOkHttp(editText_name,editText_number,editText_pwd);
+            case R.id.zhuce_btn://注册
+                String namestr=name_edit.getText().toString();
+                String pwdstr=pwd_edit.getText().toString();
+                String captcha = captcha_edit.getText().toString();
+                if (namestr.length()>0 && pwdstr.length()>0 && pwdstr.length()>=6){
+                    phone = phone_edit.getText().toString();
+                    SMSSDK.submitVerificationCode("+86",phone,captcha);
+                }
                 break;
-            case R.id.up_codebt:
-                time.start();//开始计时
+            case R.id.captcha_btn:
+                phone = phone_edit.getText().toString();
+                if (phone.length()>10){
+                    SMSSDK.getVerificationCode("+86",phone);
+                    time.start();//开始计时
+                }
         }
     }
     public void registerWithOkHttp(final String name, String number, String pwd){
@@ -97,9 +142,9 @@ public class RegisterActivity extends Activity implements View.OnClickListener {
                     @Override
                     public void run() {
                         if (name1.equals("success")){
-                            Toast.makeText(RegisterActivity.this,"注册成功"+responseData,Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegisterActivity.this,"注册成功",Toast.LENGTH_SHORT).show();
                         }else {
-                            Toast.makeText(RegisterActivity.this,"注册失败"+responseData,Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegisterActivity.this,"注册失败",Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
@@ -121,6 +166,21 @@ public class RegisterActivity extends Activity implements View.OnClickListener {
         client.newCall(request).enqueue(callback);
     }
 
+    private void init() {
+        backLoginPage=findViewById(R.id.back_login_img);
+        backLoginPage.setOnClickListener(this);
+        name_edit=findViewById(R.id.name_edit);//获取注册名字
+        phone_edit=findViewById(R.id.phone_edit);//获取注册手机号码
+        pwd_edit=findViewById(R.id.pwd_edit);//获取注册密码
+        zhuce_btn=findViewById(R.id.zhuce_btn);//注册按钮
+        zhuce_btn.setOnClickListener(this);//注册按钮点击事件
+        captcha_btn=findViewById(R.id.captcha_btn);//获取验证码按钮
+        captcha_btn.setOnClickListener(this);
+        time=new TimeCount(60000,1000);
+        constant=new Constant();//实例化常量类
+        captcha_edit = findViewById(R.id.captcha_edit);//验证码输入框
+    }
+
     /*短信验证按钮倒计时*/
     class TimeCount extends CountDownTimer {
         public TimeCount(long millisInFuture, long countDownInterval) {
@@ -130,19 +190,23 @@ public class RegisterActivity extends Activity implements View.OnClickListener {
         @Override
         /*倒计时过程*/
         public void onTick(long millisUntilFinished) {
-            identifying.setClickable(false);//防止重复点击
-            identifying.setText(millisUntilFinished / 1000 + "s");
-            identifying.setBackgroundResource(R.drawable.bg_click);
+            captcha_btn.setClickable(false);//防止重复点击
+            captcha_btn.setText(millisUntilFinished / 1000 + "s");
+            captcha_btn.setBackgroundResource(R.drawable.bg_click);
         }
 
         @Override
         /*倒计时结束*/
         public void onFinish() {
-            identifying.setText("获取验证码");
-            identifying.setClickable(true);
-            identifying.setBackgroundResource(R.drawable.bg_message);
+            captcha_btn.setText("获取验证码");
+            captcha_btn.setClickable(true);
+            captcha_btn.setBackgroundResource(R.drawable.bg_message);
         }
     }
 
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SMSSDK.unregisterEventHandler(eventHandler);//结束回收
     }
+}
