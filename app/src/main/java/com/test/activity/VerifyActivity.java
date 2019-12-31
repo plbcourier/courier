529e1e8188b2d8121a176a564e372ad73e513138
QUANBOWEN <1064110185@qq.com>
2019-12-31 10:53:56 +0800

    全博文
    身份证照片上传

--- app/src/main/java/com/test/activity/VerifyActivity.java
+++ app/src/main/java/com/test/activity/VerifyActivity.java
@@ -5,10 +5,13 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Environment;
 import android.os.StrictMode;
@@ -28,16 +31,24 @@ import android.widget.Toast;
 
 import com.test.courier.R;
 import com.test.entity.Constant;
+import com.test.sqlite.UserinfoDBUtil;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -45,7 +56,6 @@ import okhttp3.Response;
 
 public class VerifyActivity extends AppCompatActivity implements View.OnClickListener{
     private Constant constant;
-    private EditText editText;//骑手ID
     private ImageView view;//身份证正面
     private ImageView view1;//身份证反面
     private Button button;//上传按钮
@@ -58,7 +68,6 @@ public class VerifyActivity extends AppCompatActivity implements View.OnClickLis
 
     private Bitmap zhengmian;//身份证正面
     private Bitmap fanmian;//身份证反面
-    private String text;//骑手id的值
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,8 +81,7 @@ public class VerifyActivity extends AppCompatActivity implements View.OnClickLis
         view=findViewById(R.id.view);//正面
         view1=findViewById(R.id.view1);//反面
         button=findViewById(R.id.button);//上传
-        editText=findViewById(R.id.text);//骑手id
-
+        constant = new Constant();
 
 
         view.setOnClickListener(this);
@@ -97,10 +105,22 @@ public class VerifyActivity extends AppCompatActivity implements View.OnClickLis
                 takePhotoOrSelecctPicture();
                 break;
             case R.id.button:
-                //获取具体的值
-                text=editText.getText().toString();//获取骑手id的值
+                /*
+mImageView.setDrawingCacheEnabled(true);
+Bitmap bitmap = Bitmap.createBitmap(mImageView.getDrawingCache());
+mImageView.setDrawingCacheEnabled(false);*/
+                view.setDrawingCacheEnabled(true);
                 zhengmian=((BitmapDrawable)view.getDrawable()).getBitmap();//获取身份证正面图片
+                view.setDrawingCacheEnabled(false);
+
+                view1.setDrawingCacheEnabled(true);
                 fanmian=((BitmapDrawable)view1.getDrawable()).getBitmap();//获取身份证反面图片
+                view1.setDrawingCacheEnabled(false);
+
+                File image=getFile(zhengmian);
+                File image1=getFile(fanmian);
+                UploadIDcard uploadIDcard=new UploadIDcard();
+                uploadIDcard.execute(image,image1);
                 break;
 
         }
@@ -128,6 +148,7 @@ public class VerifyActivity extends AppCompatActivity implements View.OnClickLis
                                 }
                                 //获取图片文件的uri对象
                                 imageUri =Uri.fromFile(takePhotoImage);
+
                                 //创建一个Intent用于启动手机的照相拍照功能
                                 Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                                 //指定输出到文件uri中
@@ -234,9 +255,96 @@ public class VerifyActivity extends AppCompatActivity implements View.OnClickLis
                 break;
         }
     }
+    //用于获取
+    private String getUserid(){//获取当前登录用户id
+        UserinfoDBUtil userinfoDBUtil = new UserinfoDBUtil();//userinfo数据库工具类
+        SQLiteDatabase database = userinfoDBUtil.getSqLiteDatabase(VerifyActivity.this);//获取userinfo数据库
+        //查询当前登录用户的userid
+        Cursor cursor = database.query("userinfo",null,null,null,null,null,null);
+        cursor.moveToFirst();
+        String userid = cursor.getString(1);
+        return userid;
+    }
+    //将获取的图片转化成file
+    public File getFile(Bitmap bitmap) {//将图片转成file
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, baos);
+        File file = new File(Environment.getExternalStorageDirectory() + "/temp.jpg");
+        try {
+            file.createNewFile();
+            FileOutputStream fos = new FileOutputStream(file);
+            InputStream is = new ByteArrayInputStream(baos.toByteArray());
+            int x = 0;
+            byte[] b = new byte[1024 * 100];
+            while ((x = is.read(b)) != -1) {
+                fos.write(b, 0, x);
+            }
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
 
+    //AsyncTask三个参数，第一个File是执行前调用时返回值，Void则是中途返回值，String则是结束后返回值
+    public class UploadIDcard extends AsyncTask<File,Void,String>{
 
+        @Override
+        protected String doInBackground(File... files) {
+            //获取具体的值
+            String userid = getUserid();//骑手id的值
+            OkHttpClient client=new OkHttpClient();
+            MultipartBody.Builder multipartBody = new MultipartBody.Builder().setType(MultipartBody.FORM);
+            if (files[0]!=null && files[1]!=null){
+                // MediaType.parse() 里面是上传的文件类型。
+                RequestBody requestBody = RequestBody.create(MediaType.parse("image/*"),files[0]);
+                RequestBody requestBody1 = RequestBody.create(MediaType.parse("image/*"),files[1]);
+                //获取文件名称
+                String filename = files[0].getName();
+                String filename1 = files[1].getName();
+                // 参数分别为， 请求key ，文件名称 ， RequestBody
+                multipartBody
+                        .addFormDataPart("id",userid)
+                        .addFormDataPart("files",filename,requestBody)
+                        .addFormDataPart("files",filename1,requestBody1);
 
+            }
+            Request request = new Request.Builder()
+                    .url(constant.PREFIX+constant.UPLOADCARD)
+                    .post(multipartBody.build())
+                    .build();
+            client.newBuilder().readTimeout(3000, TimeUnit.MILLISECONDS)
+                    .build()
+                    .newCall(request)
+                    .enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(VerifyActivity.this, "上传失败", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(VerifyActivity.this, "上传中，请稍后查看", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    });
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 
 
 
