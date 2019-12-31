c360d0e7901773d07919643e622504b6ce4122c6
yks <1679682913@qq.com>
2019-12-31 11:02:55 +0800

    颜恺生 修改依赖

--- app/src/main/java/com/test/jpush/JPushApplication.java
+++ app/src/main/java/com/test/jpush/JPushApplication.java
@@ -1,6 +1,8 @@
 package com.test.jpush;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 import android.util.Log;
 
 import cn.jpush.android.api.JPushInterface;
@@ -17,5 +19,11 @@ public class JPushApplication extends Application {
         JPushInterface.init(getApplicationContext());
         String registrationId = JPushInterface.getRegistrationID(getApplicationContext());
         Log.e("tag",registrationId);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 }
